62dd94f7918021a245065cd914fac5c5dc3a834c
eyehatekyle <62825793+eyehatekyle@users.noreply.github.com>
2022-07-04 23:50:57 -0600

    Welcome Text for webdev.html

--- js/webdev.js
+++ js/webdev.js
@@ -1,1 +1,47 @@
-alert('Warning: Test site is test.');
+// alert('Warning: Test site is test.');
+
+let myButton = document.getElementById('changeuser');
+let myHeading = document.querySelector('h3')
+
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow < 12){
+  greeting = 'Good Morning, ';
+}
+else if (hourNow < 18){
+  greeting = 'Good afternoon, ';
+}
+else if (hourNow < 24){
+  greeting = 'Good evening, '; 
+}
+else {
+  greeting = "Welcome, ";
+}
+
+function setUserName() {
+	let myName = prompt('Please enter your name.');
+	if(!myName) {
+		setUserName();
+	} else{
+		localStorage.setItem('name', myName);
+		myHeading.innerHTML = greeting + myName + '.';
+	}
+
+}
+
+if (!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	let storedName = localStorage.getItem('name');
+	myHeading.innerHTML = greeting + storedName + '.';
+}
+
+myButton.onclick = function() {
+	setUserName();
+}
+
+
+
+
